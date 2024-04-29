4389b17af2b5fcc6535b25cef972a4e351009004
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2024-04-29 10:19:46 +0100

    String differences

--- code/seq_match.py
+++ code/seq_match.py
@@ -84,6 +84,7 @@ def compare(form1, form2):
 	if form1 == form2:
 		return transformations
 	seq_matcher.set_seqs(form1, form2)
+	print(seq_matcher.get_opcodes())
 	for code, f1s, f1e, f2s, f2e in seq_matcher.get_opcodes():
 		if code == 'replace':
 			part1 = form1[f1s:f1e]
@@ -101,10 +102,51 @@ def compare(form1, form2):
 
 
 
+# def compare(form1, form2):
+# 	transformations = []
+# 	if form1 == form2:
+# 		return transformations
+# 	seq_matcher.set_seqs(form1, form2)
+# 	for code, f1s, f1e, f2s, f2e in seq_matcher.get_opcodes():
+# 		if code == 'replace':
+# 			part1 = form1[f1s:f1e]
+# 			part2 = form2[f2s:f2e]
+# 			transformations.append(f'{part1}→{part2}')
+# 		elif code == 'delete':
+# 			deletion = form1[f1s:f1e]
+# 			# part1, part2 = delete(deletion, form2[f2s-1:f2s], form2[f2s:f2s+1])
+# 			transformations.append(f'-{deletion}')
+# 		elif code == 'insert':
+# 			insertion = form2[f2s:f2e]
+# 			# part1, part2 = insert(insertion, form1[f1s-1:f1s], form1[f1s:f1s+1])
+# 			transformations.append(f'+{insertion}')
+# 	return transformations
+
+
+
+def shortest_replacement(a, b):
+	# Pad the shorter string with placeholder characters
+	if len(a) < len(b):
+		a = a.ljust(len(b), '\0')
+	elif len(b) < len(a):
+		b = b.ljust(len(a), '\0')
+	print(a, b)
+	matcher = SequenceMatcher(None, a, b)
+	opcodes = matcher.get_opcodes()
+	print(opcodes)
+	
+	replacements = [(a[i1:i2], b[j1:j2]) for tag, i1, i2, j1, j2 in opcodes if tag == 'replace']
+	
+	shortest_replacement = min(replacements, key=lambda x: len(x[0]))
+	
+	return shortest_replacement
+
+
+print(shortest_replacement('cyng', 'cyning'))
+quit()
+
+
 assert compare('publique', 'publick')[0] == 'que→ck'
 assert compare('delicius', 'delicious')[0] == 'u→ou'
 assert compare('delicious', 'deliciouss')[0] == 's→ss'
 assert compare('delicious', 'deliciouse')[0] == '$→e$'
-
-
-
