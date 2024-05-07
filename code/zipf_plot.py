baf019f35f9477892a1c1753d6ce7c21e991ce96
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2024-05-07 13:44:47 +0100

    Recreate freq plot

--- code/zipf_plot.py
+++ code/zipf_plot.py
@@ -4,48 +4,49 @@ import pandas as pd
 from scipy.stats import rankdata
 import matplotlib.pyplot as plt
 
+plt.rcParams.update({'font.sans-serif': 'Helvetica Neue', 'font.size': 7})
+
 ROOT = Path(__file__).parent.parent.resolve()
 DATA = ROOT / 'data'
 
-count_corpus = pd.read_csv(DATA / 'count_corpus_texts.csv')
+count_texts = pd.read_csv(DATA / 'count_corpus_texts.csv')
+count_corpus = pd.read_csv(DATA / 'count_corpus.csv')
 count_oed = pd.read_csv(DATA / 'count_oed.csv')
 
 
-fig, axes = plt.subplots(3, 5, figsize=(5, 3), sharey=True)
-axes = axes.flatten()
 
-for band_i in range(1, 14):
-
-	axis = axes[band_i - 1]
-
-	corpus_totals = count_corpus.groupby('lemma')[f'band_{band_i}'].sum().to_numpy()
-	oed_totals = count_oed.groupby('lemma')[f'band_{band_i}'].sum().to_numpy()
-
-	lemma = count_oed['lemma'].unique()
-
-	oed_ranks = rankdata(-(oed_totals + corpus_totals), method='ordinal')
-
-	oed_means = []
-	cor_means = []
-
-	for i in range(0, len(oed_ranks), 100):
-		indices_in_rank_band = np.where((oed_ranks >= i) & (oed_ranks < (i + 100)))
-		oed_means.append(oed_totals[indices_in_rank_band].mean())
-		cor_means.append(corpus_totals[indices_in_rank_band].mean())
-
-	end_rank_index = oed_means.index(0)
-
-	x = [i * 100 + 1 for i, _ in enumerate(oed_means[:end_rank_index])]
-
-	axis.plot(x, oed_means[:end_rank_index], color='cadetblue')
-	axis.plot(x, cor_means[:end_rank_index], color='crimson')
-
-	# axis.set_xscale('log')
+def freq_by_rank_plot(axis, freqs, title, color, label):
+	freqs = freqs[ np.where(freqs > 0) ]
+	sorted_freqs = sorted(freqs, reverse=True)
+	axis.plot(range(1, len(sorted_freqs) + 1), sorted_freqs, color=color, label=label)
+	axis.set_xscale('log')
 	axis.set_yscale('log')
+	axis.set_ylim(1, 100000)
+	axis.set_xlim(1, 10000)
+	axis.set_yticks([1, 10, 100, 1000, 10000, 100000])
+	axis.set_xticks([1, 10, 100, 1000, 10000, 100000])
+	axis.set_title(title, fontsize=7)
+	axis.set_xlabel('Rank')
 
-axes[13].axis('off')
-axes[14].axis('off')
 
-fig.tight_layout(pad=0.1, h_pad=0.1, w_pad=0.1)
 
-plt.show()
+fig, axes = plt.subplots(1, 4, figsize=(7.48, 1.8), sharey=True, sharex=True)
+
+BROAD_BANDS = {'OE': 'Old English', 'ME': 'Middle English', 'eME': 'Early Modern English', 'lME': 'Late Modern English'}
+
+for band, axis in zip(BROAD_BANDS, axes):
+
+	oed_totals = count_oed.groupby('lemma')[band].sum().to_numpy()
+	corpus_totals = count_corpus.groupby('lemma')[band].sum().to_numpy()
+	text_totals = count_texts.groupby('lemma')[band].sum().to_numpy()
+
+	freq_by_rank_plot(axis, oed_totals, BROAD_BANDS[band], 'cadetblue', 'N quotes')
+	freq_by_rank_plot(axis, text_totals, BROAD_BANDS[band], 'darkorange', 'N texts')
+	freq_by_rank_plot(axis, corpus_totals, BROAD_BANDS[band], 'crimson', 'N occurrences')
+	
+
+axes[0].set_ylabel('Frequency')
+axes[0].legend(frameon=False, markerfirst=False)
+
+fig.tight_layout(pad=0.5, h_pad=0.5, w_pad=0.5)
+fig.savefig(ROOT / 'manuscript' / 'figs' / 'zipf_plots.pdf')
